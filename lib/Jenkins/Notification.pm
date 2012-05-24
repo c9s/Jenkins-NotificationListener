8df755e29049e23b95eeaa571977532d634a3eb9
c9s <cornelius.howl@gmail.com>
2012-05-25 01:16:40 +0800

    extract status, phase

--- lib/Jenkins/Notification.pm
+++ lib/Jenkins/Notification.pm
@@ -16,6 +16,10 @@ has build => ( is => 'rw' , isa => 'Net::Jenkins::Job::Build' );
 
 has job => ( is => 'rw', isa => 'Net::Jenkins::Job' );
 
+has status => ( is => 'rw' , isa => 'Str' );
+
+has phase => ( is => 'rw' , isa => 'Str' );
+
 has parameters => ( is => 'rw' );
 
 sub BUILDARGS {
@@ -27,6 +31,9 @@ sub BUILDARGS {
     my $build_url = $build_args->{fullUrl};
 
     $args{build} = build_build_object $build_url;
+
+    $args{status} = $build_args->{status};
+    $args{phase} = $build_args->{phase};
 
     # use Data::Dumper; warn Dumper( \%args );
     # build JOB, BUILD objects
