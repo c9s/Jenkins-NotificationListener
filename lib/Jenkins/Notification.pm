597a3cc81f7c2cd04b84ac6778e8a607f59fb5f1
c9s <cornelius.howl@gmail.com>
2012-05-25 01:14:49 +0800

    Add Jenkins::Notification

--- lib/Jenkins/Notification.pm
+++ lib/Jenkins/Notification.pm
@@ -2,6 +2,7 @@ package Jenkins::Notification;
 use warnings;
 use strict;
 use Moose;
+use Net::Jenkins::Utils qw(build_job_object build_build_object);
 use Net::Jenkins::Job;
 use Net::Jenkins::Job::Build;
 
@@ -11,17 +12,24 @@ has name => ( is => 'rw' , isa => 'Str' );
 # job url
 has url => ( is => 'rw', isa => 'Str' );
 
-has build => ( is => 'rw' );
+has build => ( is => 'rw' , isa => 'Net::Jenkins::Job::Build' );
 
-has job => ( is => 'rw' );
+has job => ( is => 'rw', isa => 'Net::Jenkins::Job' );
+
+has parameters => ( is => 'rw' );
 
 sub BUILDARGS {
     my ($self,%args) = @_;
 
-    use Data::Dumper; warn Dumper( \%args );
+    $args{job} = build_job_object $args{url};
 
+    my $build_args = delete $args{build};
+    my $build_url = $build_args->{fullUrl};
+
+    $args{build} = build_build_object $build_url;
+
+    # use Data::Dumper; warn Dumper( \%args );
     # build JOB, BUILD objects
-
     return \%args;
 }
 
