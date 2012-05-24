ee0bcebdd300020ca0b500c67030ba6f6ff4f810
c9s <cornelius.howl@gmail.com>
2012-05-25 00:13:14 +0800

    Provide a basic test

--- lib/Jenkins/NotificationListener.pm
+++ lib/Jenkins/NotificationListener.pm
@@ -3,6 +3,48 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use Net::Jenkins;
+use Net::Jenkins::Job;
+use Net::Jenkins::Job::Build;
+use AnyEvent::Socket;
+use Moose;
+use methods;
+use JSON::XS;
+use Try::Tiny;
+
+has host => (is => 'rw');
+
+has port => (is => 'rw', isa => 'Int');
+
+has on_notify => (is => 'rw');
+
+has on_error => (is => 'rw', default => sub { 
+    return sub { warn @_; };
+});
+
+method start {
+    tcp_server $self->host, $self->port , sub {
+        my ($fh, $host, $port) = @_;
+        my $content = '';
+        my $buf = '';
+        while( my $bytes = sysread $fh, $buf, 1024 ) {
+            $content .= $buf;
+        }
+
+        eval {
+            if( $content ) {
+                my $json = decode_json $content;
+                $self->on_notify->( $json );
+            } else {
+                die 'Empty content';
+            }
+        };
+        if ( $@ ) {
+            $self->on_error->( $@ );
+        }
+    };
+}
+
 1;
 __END__
 
---
+++ xt/04_listener.t
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+use Test::More;
+use Jenkins::NotificationListener;
+use AnyEvent::Socket;
+use Time::HiRes qw(usleep);
+
+my $cv = AnyEvent->condvar;
+
+Jenkins::NotificationListener->new( host => undef , port => 8888 , on_notify => sub {
+    my $payload = shift;   # Jenkins::Notification;
+    $cv->send;
+
+    ok $payload;
+})->start;
+
+usleep 1000;
+
+tcp_connect "localhost", 8888, sub {
+    my ($fh) = @_
+        or die "localhost failed: $!";
+
+
+    print $fh <<'JSON';
+{
+    "name":"JobName",
+    "url":"JobUrl",
+    "build":{
+        "number": 1,
+        "phase": "STARTED",
+        "status": "FAILED",
+        "url":"job/project/5",
+        "fullUrl":"http://ci.jenkins.org/job/project/5",
+        "parameters":{
+            "branch":"master"
+        }
+    }
+}
+JSON
+
+    # enjoy your filehandle
+};
+
+$cv->recv;
+
+
+done_testing;
